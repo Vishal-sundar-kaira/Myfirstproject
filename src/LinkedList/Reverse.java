b9543ac3ba2b0bbcf235bbe2fdbc2c2e295ee18d
vishal kaira <kairavishal37@gmail.com>
2023-07-10 18:50:30 +0300

    Completed reverse of linkedlist.

---
+++ src/LinkedList/Reverse.java
@@ -0,0 +1,60 @@
+package LinkedList;
+import java.util.*;
+public class Reverse {
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data=d;
+            next=null;
+        }
+    }
+    public static Node reverse(Node head){
+        Node prev=null;
+        Node current=head;
+        Node next=null;
+        while(current!=null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        head=prev;
+        return head;
+    }
+    public static Node buildLinkedList(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first element");
+        int initial=sc.nextInt();
+        Node start = new Node(initial);
+        Node current = start;
+
+        // System.out.print("Enter the number of elements: ");
+        // int n = scanner.nextInt();
+
+        System.out.print("Enter rest elements: ");
+        int data=sc.nextInt();
+        while(data!=-1) {
+            current.next = new Node(data);
+            current = current.next;
+            data = sc.nextInt();
+        }
+
+        return start;
+    }
+    public static void printLinkedList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node head = buildLinkedList();
+        // Node newNode=new Node();
+        printLinkedList(head);
+        Node newhead=reverse(head);
+        printLinkedList(newhead);
+    }
+}
