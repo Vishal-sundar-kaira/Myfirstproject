544e1272c2934ff9ec6bb1971d6820e0af0347c9
vishal kaira <kairavishal37@gmail.com>
2023-07-18 19:59:47 +0300

    Completed expression reverse using stack.

--- src/Array/Expressionprac.java
+++ src/Array/Expressionprac.java
@@ -1,5 +1,39 @@
 package Array;
-
+import java.util.*;
 public class Expressionprac {
     //practicing expression
+    public static int parenthes(String s){
+        Stack<Character> st=new Stack<Character>();
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='{'){
+                st.add(s.charAt(i));
+            }
+            else {
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+                else{
+                    st.add('{');
+                    ans++;
+                }
+            }
+        }
+        if(st.size()%2!=0){
+            return -1;
+        }
+        else{
+            ans+=st.size()/2;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println("enter parenthesis");
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int a=parenthes(s);
+        System.out.println(a);
+    }
+    
+
 }
